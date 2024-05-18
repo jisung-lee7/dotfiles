1bcc9f2360d9ab85428f7313001ab8b192c45853
Jisung Lee <jskurt9352@gmail.com>
2024-05-18 15:03:54 +0900

    update replace in keymap

--- .nvim/lua/custom/keymaps.lua
+++ .nvim/lua/custom/keymaps.lua
@@ -74,8 +74,8 @@ vim.cmd [[
   xnoremap <Leader>cl y:<C-u>silent put=['console.log(\"<C-r>"\", <C-r>");']<CR>-2==+
 
   " replace
-  nnoremap <Leader>rp :%s///g<Left><Left>
-  nnoremap <Leader>rc :%s///gc<Left><Left><Left>
-  xnoremap <Leader>rp :%s///g<Left><Left>
-  xnoremap <Leader>rc :%s///gc<Left><Left><Left>
+  nnoremap <Leader>rp :%s/<C-r><C-w>//g<Left><Left>
+  nnoremap <Leader>rc :%s/<C-r><C-w>//gc<Left><Left><Left>
+  xnoremap <Leader>rp y:%s/\V<C-r>=escape(@", '/\')<CR>//g<Left><Left>
+  xnoremap <Leader>rc y:'<,'>%s/\V<C-r>=escape(@", '/\')<CR>//gc<Left><Left><Left>
 ]]
