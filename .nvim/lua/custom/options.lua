13e1b811d8c1c8fe57ee266fa793812a6658bbbe
Jisung Lee <jskurt9352@gmail.com>
2024-05-27 15:07:53 +0900

    set scrolloff to 5

--- .nvim/lua/custom/options.lua
+++ .nvim/lua/custom/options.lua
@@ -30,6 +30,7 @@ local options = {
   numberwidth = 4,                        -- set number column width to 2 {default 4}
   signcolumn = "yes",                     -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                           -- display lines as one long line
+  scrolloff = 5,
 }
 
 vim.loader.enable()
