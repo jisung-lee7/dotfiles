4e75fa485430144555e342fdc0b65633c095c3b5
Jisung Lee <jskurt9352@gmail.com>
2024-06-13 06:56:41 +0900

    set S-Tab of vimwiki

--- .nvim/lua/custom/vimwiki.lua
+++ .nvim/lua/custom/vimwiki.lua
@@ -1,15 +1,19 @@
 local M = {
-  "vimwiki/vimwiki",
+	"vimwiki/vimwiki",
 }
 
 M.init = function()
-  vim.g.vimwiki_folding = ""
-  vim.g.vimwiki_list = { { path = "~/vimwiki/", syntax = "markdown", ext = ".md" } }
-  vim.g.vimwiki_ext2syntax = {
-    [".md"] = "markdown",
-    [".markdown"] = "markdown",
-    [".mdown"] = "markdown",
-  }
+	vim.g.vimwiki_folding = ""
+	vim.g.vimwiki_list = { { path = "~/vimwiki/", syntax = "markdown", ext = ".md" } }
+	vim.g.vimwiki_ext2syntax = {
+		[".md"] = "markdown",
+		[".markdown"] = "markdown",
+		[".mdown"] = "markdown",
+	}
+
+	vim.cmd([[
+  autocmd FileType vimwiki inoremap <buffer> <S-Tab> <C-d>
+]])
 end
 
 return M
