1d8927c5a640a8f85407e48d74a619f7e376acc2
Jisung Lee <jskurt9352@gmail.com>
2024-07-09 02:28:26 +0900

    update format indent-blankline

--- .nvim/lua/custom/indent-blankline.lua
+++ .nvim/lua/custom/indent-blankline.lua
@@ -1,5 +1,14 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {},
+local M = {
+	"lukas-reineke/indent-blankline.nvim",
 }
+
+M.config = function()
+	local ibl = require("ibl")
+	local setup = {
+		-- scope = { show_start = true, show_end = true },
+	}
+
+	ibl.setup(setup)
+end
+
+return M
