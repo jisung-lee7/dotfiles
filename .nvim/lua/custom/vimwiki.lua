4d3e9b35109bbb9a853aa7a65765106e5aff105a
Jisung Lee <jskurt9352@gmail.com>
2024-07-17 10:00:54 +0900

    update about vimwiki

--- .nvim/lua/custom/vimwiki.lua
+++ .nvim/lua/custom/vimwiki.lua
@@ -4,7 +4,7 @@ local M = {
 
 M.init = function()
 	vim.g.vimwiki_folding = ""
-	vim.g.vimwiki_list = { { path = "~/vimwiki/", syntax = "markdown", ext = ".md" } }
+	vim.g.vimwiki_list = { { path = "~/jisung-lee-wiki/vimwiki/", syntax = "markdown", ext = ".md" } }
 	vim.g.vimwiki_ext2syntax = {
 		[".md"] = "markdown",
 		[".markdown"] = "markdown",
