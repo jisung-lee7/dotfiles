dd61a7806bd1dde45ab65d237a9181ac2e1e4518
Jisung Lee <jskurt9352@gmail.com>
2025-01-05 15:42:09 +0900

    replace indent highlighter to snacks

---
+++ .nvim/lua/custom/snacks.lua
@@ -0,0 +1,21 @@
+local M = {
+	"folke/snacks.nvim",
+}
+M.config = function()
+	local snacks = require("snacks")
+	local setup = {
+		indent = {
+			priority = 1,
+			enabled = true,
+			char = "│",
+			only_scope = false, -- only show indent guides of the scope
+			only_current = false, -- only show indent guides in the current window
+			hl = "SnacksIndent", ---@type string|string[] hl groups for indent guides
+			animate = {
+				enabled = false,
+			},
+		},
+	}
+	snacks.setup(setup)
+end
+return M
