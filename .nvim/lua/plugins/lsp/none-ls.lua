2eeea9f049d0cce55951d58a8813d7af88f553cc
Jisung Lee <jskurt9352@gmail.com>
2026-08-12 21:53:40 +0900

    fix(JS): replace deprecated client.supports to client:supports

--- .nvim/lua/plugins/lsp/none-ls.lua
+++ .nvim/lua/plugins/lsp/none-ls.lua
@@ -26,7 +26,7 @@ M.config = function()
         return
       end
 
-      if client.supports_method "textDocument/formatting" then
+      if client:supports_method "textDocument/formatting" then
         vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
         vim.api.nvim_create_autocmd("BufWritePre", {
           group = augroup,
